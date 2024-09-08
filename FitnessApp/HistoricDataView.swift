b30af86f9b646b311f880ff6e3357837e40888a4
Mehmet Akif Kaymak <m.akif.kaymak@hotmail.com>
2024-09-08 17:31:46 +0300

    Home Header and Progress Circle

---
+++ FitnessApp/HistoricDataView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricDataView.swift
+//  FitnessApp
+//
+//  Created by Mehmet Akif Kaymak on 8.09.2024.
+//
+
+import SwiftUI
+
+struct HistoricDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoricDataView()
+}
---
+++ FitnessApp/ProgressCircleView.swift
@@ -0,0 +1,33 @@
+//
+//  ProgressCircleView.swift
+//  FitnessApp
+//
+//  Created by Mehmet Akif Kaymak on 8.09.2024.
+//
+
+import SwiftUI
+
+struct ProgressCircleView: View {
+    @Binding var progress: Int
+    var goal: Int
+    var color: Color
+    private let width: CGFloat = 20
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(color.opacity(0.3), lineWidth: 20)
+            
+            Circle()
+                .trim(from: 0, to: CGFloat(progress) / CGFloat(goal))
+                .stroke(color, style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+                .shadow(radius: 5)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProgressCircleView(progress: .constant(100), goal: 200, color: .red)
+}
