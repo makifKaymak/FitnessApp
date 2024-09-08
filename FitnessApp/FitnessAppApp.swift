afdb45d0d1189b8ec8456718b5e5b4874db01c06
Mehmet Akif Kaymak <m.akif.kaymak@hotmail.com>
2024-09-08 14:44:50 +0300

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Mehmet Akif Kaymak on 8.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
